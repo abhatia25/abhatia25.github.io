c7a08982f3d921ed5a56049e1f785939c29770a3
Aditya Bhatia <bhatia.aditya@gmail.com>
2021-06-13 02:34:43 -0400

    comment out error catching code

--- docs/contact-form-process.php
+++ docs/contact-form-process.php
@@ -5,23 +5,23 @@ if (isset($_POST['Email'])) {
     $email_to = "bhatia.aditya@gmail.com";
     $email_subject = "New contact form submissions";
 
-    function problem($error)
-    {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])
-    ) {
-        problem('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
+    // function problem($error)
+    // {
+    //     echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+    //     echo "These errors appear below.<br><br>";
+    //     echo $error . "<br><br>";
+    //     echo "Please go back and fix these errors.<br><br>";
+    //     die();
+    // }
+    //
+    // // validation expected data exists
+    // if (
+    //     !isset($_POST['name']) ||
+    //     !isset($_POST['email']) ||
+    //     !isset($_POST['message'])
+    // ) {
+    //     problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    // }
 
     $name = $_POST['name']; // required
     $email = $_POST['email']; // required
